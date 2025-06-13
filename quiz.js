8636f3d728973a9edec2c2237b9e98c98c7716ad
Blessing <eblessing793577@gmail.com>
2025-06-13 11:13:37 +0100

    Created a simple quiz

--- quiz.js
+++ quiz.js
@@ -4,7 +4,7 @@ function checkAnswer() {
 
   if (!userAnswer) {
     document.getElementById('feedback').textContent = "Please, select an answer!";
-  } else if (userAnswer === correctAnswer) {
+  } else if (userAnswer.value === correctAnswer) {
     document.getElementById('feedback').textContent = "Correct! Well done.";
   } else {
     document.getElementById('feedback').textContent = "That's incorrect. Try again!";
