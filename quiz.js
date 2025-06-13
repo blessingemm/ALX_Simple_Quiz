949ce051a00a1cd8e0d97d17c4c3c91023b2be90
Blessing <eblessing793577@gmail.com>
2025-06-13 11:17:35 +0100

    Created a simple quiz

--- quiz.js
+++ quiz.js
@@ -2,6 +2,10 @@ function checkAnswer() {
   const correctAnswer = "4";
   const userAnswer = document.querySelector('input[name="quiz"]:checked');
 
+  if (userAnswer === correctAnswer){
+    
+  }
+
   if (!userAnswer) {
     document.getElementById('feedback').textContent = "Please, select an answer!";
   } else if (userAnswer.value === correctAnswer) {
